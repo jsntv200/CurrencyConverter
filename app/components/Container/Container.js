1c3016f6a361bffda9f4585801e48deefc4f22ff
Jason Taylor <jason@redant.com.au>
2017-09-19 19:02:03 +1000

    Home screen, Container & logo component

---
+++ app/components/Container/Container.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import PropTypes from 'prop-types';
+
+import styles from './styles';
+
+const Container = ({ children }) => <View style={styles.container}>{children}</View>;
+
+Container.propTypes = {
+  children: PropTypes.any,
+};
+
+export default Container;
