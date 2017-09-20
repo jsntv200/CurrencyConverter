1836d8ccc25f683db75ae0961bc1c69c76099e7a
Jason Taylor <jason@redant.com.au>
2017-09-20 22:41:14 +1000

    Home Screen: Button

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -4,6 +4,7 @@ import { StatusBar } from 'react-native';
 import { Container } from '../components/Container';
 import { Logo } from '../components/Logo';
 import { InputWithButton } from '../components/TextInput';
+import { ClearButton } from '../components/Buttons';
 
 const TEMP_BASE_CURRENCY = 'USD';
 const TEMP_QUOTE_CURRENCY = 'GBP';
@@ -21,6 +22,10 @@ class Home extends Component {
 
   handleTextChange = (text) => {
     console.log('change text', text);
+  };
+
+  handleSwapCurrency = () => {
+    console.log('press swap currency');
   };
 
   render() {
@@ -42,6 +47,7 @@ class Home extends Component {
           editable={false}
           keyboardType="numeric"
         />
+        <ClearButton text="Reverse Currency" onPress={this.handleSwapCurrency} />
       </Container>
     );
   }
