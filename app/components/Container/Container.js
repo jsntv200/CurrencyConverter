3f2e7ced3ec119a384f1bf917f28d29b0246d09b
Jason Taylor <jason@redant.com.au>
2017-09-20 20:36:17 +1000

    Home Screen: Closing the Keyboard

--- app/components/Container/Container.js
+++ app/components/Container/Container.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import PropTypes from 'prop-types';
 
 import styles from './styles';
 
-const Container = ({ children }) => <View style={styles.container}>{children}</View>;
+const Container = ({ children }) => (
+  <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+    <View style={styles.container}>{children}</View>
+  </TouchableWithoutFeedback>
+);
 
 Container.propTypes = {
   children: PropTypes.any,
