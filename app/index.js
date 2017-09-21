4716616c519eb4ae9366a6df396b12158ff5507b
Jason Taylor <jason@redant.com.au>
2017-09-21 16:39:02 +1000

    Currency List Screen: FlatList

--- app/index.js
+++ app/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
-import Home from './screens/Home';
+// import Home from './screens/Home';
+import CurrencyList from './screens/CurrencyList';
 
 EStyleSheet.build({
   $border: '#E2E2E2',
@@ -11,4 +12,4 @@ EStyleSheet.build({
   $white: '#FFFFFF',
 });
 
-export default () => <Home />;
+export default () => <CurrencyList />;
---
+++ app/screens/CurrencyList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, FlatList, View, StatusBar } from 'react-native';
+import currencies from '../data/currencies';
+
+const CurrencyList = () => (
+  <View style={{ flex: 1 }}>
+    <StatusBar barStyle="default" translucent={false} />
+    <FlatList
+      data={currencies}
+      renderItem={({ item }) => <Text>{item}</Text>}
+      keyExtractor={item => item}
+    />
+  </View>
+);
+
+export default CurrencyList;
