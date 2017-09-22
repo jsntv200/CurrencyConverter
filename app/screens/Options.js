012866362a097c62d08cabadda58118c0ad2b383
Jason Taylor <jason@redant.com.au>
2017-09-22 12:18:24 +1000

    External Linking

--- app/screens/Options.js
+++ app/screens/Options.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { ScrollView, StatusBar, Platform } from 'react-native';
+import { ScrollView, StatusBar, Platform, Linking } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 import { ListItem, Separator } from '../components/List';
@@ -19,7 +19,9 @@ class Options extends Component {
   };
 
   handleSitePress = () => {
-    console.log('press sites');
+    Linking.openURL('http://fixer.io').catch(() => {
+      alert('An error occured');
+    });
   };
 
   render() {
